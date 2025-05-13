869314ea27811ad0a9c9d7757978be54d2f3b312
Tobias Gurtzick <magic@wizardtales.com>
2025-05-13 19:02:25 +0200

    feat: add DAG and WorkerPool methods
    Signed-off-by: Tobias Gurtzick <magic@wizardtales.com>

--- index.js
+++ index.js
@@ -71,3 +71,173 @@ async function groupedConcurrency (iterator, promise, opts) {
 }
 
 module.exports = { groupedConcurrency };
+
+export const DAG = class DAG {
+  constructor () {
+    this._root = {};
+    this._dep = {};
+    this._new = [];
+  }
+
+  add (key, value, dep = null) {
+    const newDeps = new Map();
+    if (dep === null) {
+      this._root[key] = { value, s: false, exec: [] };
+      this._new.push(this._root[key]);
+    } else {
+      for (const x of dep) {
+        if (this._root[x]) {
+          this._root[x].exec.push(key);
+          newDeps.set(x, 1);
+        } else if (this._dep[x]) {
+          this._dep[x].exec.push(key);
+          newDeps.set(x, 1);
+        }
+
+        // while we can have different hirarchies of dependencies
+        // we still expect them to be strictly ordered
+        // this means if we can't find a dependency, it probably
+        // is already done
+      }
+
+      if (newDeps.size !== 0) {
+        this._dep[key] = {
+          value,
+          s: false,
+          exec: [],
+          dep: newDeps
+        };
+      } else {
+        this._root[key] = { value, s: false, exec: [] };
+      }
+    }
+  }
+
+  remove (key) {
+    if (this._root[key]) {
+      delete this._root[key];
+    }
+
+    if (this._dep[key]) {
+      delete this._dep[key];
+    }
+  }
+
+  finish (key) {
+    if (this._root[key]) {
+      for (const e of this._root[key].exec) {
+        const dp = this._dep[e];
+        dp.dep.delete(key);
+
+        if (dp.dep.size === 0) {
+          this._root[e] = this._dep[e];
+          this._new.push(this._dep[e]);
+
+          delete this._dep[e];
+        }
+      }
+
+      delete this._root[key];
+    }
+  }
+
+  /**
+   * Get newly inserted to the root element and make the new list
+   * empty again.
+   */
+  getPending () {
+    const p = this._new;
+    this._new = [];
+
+    return p;
+  }
+};
+
+export const WorkerPool = class WorkerPool {
+  constructor (size, getWork) {
+    this.size = 0;
+    this._max = size;
+    this._worker = [];
+    this._free = [];
+    this._getWork = getWork;
+
+    for (let i = 0; i < size; ++i) {
+      this._free.push(true);
+    }
+
+    this._queue = [];
+  }
+
+  async _work (w) {
+    ++this.size;
+    this._free.pop();
+    for (;;) {
+      console.log('lg', w);
+      await w().catch(() => {});
+
+      // if still work in queue continue, otherwise add free slot position
+      // back to the marker array
+      if (this._queue.length !== 0) {
+        w = this._queue.shift();
+      } else {
+        this._free.push(true);
+
+        // if a refill function is specified, now is the time to call it to
+        // get new work and push it in
+        if (typeof this._getWork === 'function') {
+          const work = await this._getWork();
+          for (const nw of work) {
+            this.add(nw);
+          }
+        }
+
+        break;
+      }
+    }
+
+    --this.size;
+  }
+
+  isRunning () {
+    return this.size !== 0;
+  }
+
+  add (w) {
+    if (this._free.length) {
+      this._work(w);
+    } else {
+      this._queue.push(w);
+    }
+  }
+};
+
+// const dt = new DAG();
+
+// dt.add('test', 'bla');
+// dt.add('rvtest', 'bla', ['notexisting', 'nono']);
+// dt.add('test2', 'bla');
+// dt.add('test3', 'bla', ['test']);
+// dt.add('test4', 'bla', ['test3', 'test2']);
+//
+// console.log(dt._dep, dt._root);
+//
+// dt.finish('test2');
+// dt.finish('test');
+// dt.finish('test3');
+//
+// console.log(dt._dep, dt._root, dt._new);
+//
+// console.log(dt.getPending(), dt._new);
+
+// const sleep = (x) => new Promise((resolve) => setTimeout(resolve, x * 1000));
+// const l = new WorkerPool(5, () => dt.getPending().map(x => x.value));
+// dt.add('test', () => sleep(1.1).then(() => dt.finish('test')));
+// dt.add('test3', () => sleep(1.2).then(() => dt.finish('test3')), ['test']);
+// dt.add('test4', () => sleep(1.3).then(() => dt.finish('test4')), ['test3']);
+// dt.add('test5', () => sleep(1.3).then(() => dt.finish('test5')), ['test3', 'test4']);
+// dt.add('test6', () => sleep(1.3).then(() => dt.finish('test6')), ['nono']);
+// dt.add('test2', () => sleep(2.3).then(() => dt.finish('test2')));
+//
+// l.add(() => sleep(1.1));
+// setInterval(() => console.log(l), 100);
+// console.log(l);
